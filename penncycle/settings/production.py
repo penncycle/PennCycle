47b5c4530c0832c1659753cebefa24c7f686f6c1
Brian <brishi@seas.upenn.edu>
2014-04-13 15:10:00 -0400

    configured static production settings

--- penncycle/settings/production.py
+++ penncycle/settings/production.py
@@ -5,7 +5,7 @@ import dj_database_url
 DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
-STATIC_URL = 'https://s3.amazonaws.com/penncycle/'
+STATIC_URL = '/static/'
 
 DATABASES = {
     'default': dj_database_url.config(default='postgres://localhost')
