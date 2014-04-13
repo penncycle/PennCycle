2f08dd7630bebbfdcb0f54d77a5e383f4b028ac2
Brian <brishi@seas.upenn.edu>
2014-04-13 16:51:56 -0400

    changed static

--- penncycle/settings/base.py
+++ penncycle/settings/base.py
@@ -37,10 +37,11 @@ MEDIA_URL = ''
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-STATIC_ROOT = PROJECT_DIR.child("static")
+STATIC_ROOT = PROJECT_DIR.child("assets")
+# STATIC_ROOT = "static"
 
 STATICFILES_DIRS = (
-    PROJECT_DIR.child("assets"),
+    PROJECT_DIR.child("static"),
 )
 
 STATICFILES_FINDERS = (
