5d85a731d9b81cb574c8b02c260ac94bdf90d21e
Brian <brishi@seas.upenn.edu>
2014-04-12 20:13:56 -0400

    fixed map on bikes, locations, and index

--- penncycle/assets/js/map.js
+++ penncycle/assets/js/map.js
@@ -10,37 +10,6 @@ $(document).ready(function() {
 
   var station_counts = {};
   var map = new google.maps.Map(document.getElementById("map"), map_options);
-
-  // $.getJSON("/mobile/bike_data/", function(bikes) {
-  //   for (var i = bikes.length - 1; i >= 0; i--) {
-  //     bike = bikes[i];
-  //     if (bike.status == "available" || bike.status == "Available") {
-  //       available_bikes.push({
-  //         name:bike.name,
-  //         location:bike.location
-  //       });        
-
-  //       lat = bike.latitude - 0.0002;
-  //       lng = bike.longitude - 0.0001;
-  //       station = bike.location;
-  //       if (station_counts[station]) {
-  //         lng += 0.0002 * (station_counts[station] % 5);
-  //         lat -= 0.0002 * Math.floor(station_counts[station] / 5);
-  //         station_counts[station] += 1;
-  //       } else {
-  //         station_counts[station] = 1;
-  //       }
-  //       pos = new google.maps.LatLng(lat, lng);
-  //       var marker = new MarkerWithLabel({
-  //         position: pos,
-  //         map: map,
-  //         icon: "https://s3.amazonaws.com/penncycle/img/bike_icon.png",
-  //         labelContent: bike.name,
-  //         labelClass: "marker"
-  //       }); 
-  //     }
-  //   }
-  // });
   var infoWindow = null;      
 
   $.getJSON("/mobile/station_data/", function(stations) {
@@ -80,10 +49,41 @@ $(document).ready(function() {
       }
     });
   });
+
   function bindInfoWindow(marker, map, infowindow, strDescription) {
     google.maps.event.addListener(marker, 'mouseover', function() {
         infowindow.setContent(strDescription);
         infowindow.open(map, marker);
     });  
   }
+  // $.getJSON("/mobile/bike_data/", function(bikes) {
+  //   for (var i = bikes.length - 1; i >= 0; i--) {
+  //     bike = bikes[i];
+  //     if (bike.status == "available" || bike.status == "Available") {
+  //       available_bikes.push({
+  //         name:bike.name,
+  //         location:bike.location
+  //       });        
+
+  //       lat = bike.latitude - 0.0002;
+  //       lng = bike.longitude - 0.0001;
+  //       station = bike.location;
+  //       if (station_counts[station]) {
+  //         lng += 0.0002 * (station_counts[station] % 5);
+  //         lat -= 0.0002 * Math.floor(station_counts[station] / 5);
+  //         station_counts[station] += 1;
+  //       } else {
+  //         station_counts[station] = 1;
+  //       }
+  //       pos = new google.maps.LatLng(lat, lng);
+  //       var marker = new MarkerWithLabel({
+  //         position: pos,
+  //         map: map,
+  //         icon: "https://s3.amazonaws.com/penncycle/img/bike_icon.png",
+  //         labelContent: bike.name,
+  //         labelClass: "marker"
+  //       }); 
+  //     }
+  //   }
+  // });
 });
