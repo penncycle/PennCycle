ff5d245dc247cf7c44c38ddfeeea2684f533f39e
Brian <brishi@seas.upenn.edu>
2014-04-13 14:08:02 -0400

    updates

--- penncycle/assets/js/map.js
+++ penncycle/assets/js/map.js
@@ -36,7 +36,7 @@ $(document).ready(function() {
         var marker = new MarkerWithLabel({
           position: pos,
           map: map,
-          icon: "https://s3.amazonaws.com/penncycle/img/station_icon.png",
+          icon: "https://s3.amazonaws.com/penncycle/img/PennCycle Logo 3 smallest-03.png",
           labelContent: station.name,
           labelClass: "marker"
         });
