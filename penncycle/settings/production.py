323b048955fa207f5f5ac99d6e437b6d71af9cb1
Brian <brishi@seas.upenn.edu>
2014-04-13 18:06:59 -0400

    modified procfile

--- penncycle/settings/production.py
+++ penncycle/settings/production.py
@@ -1,5 +1,5 @@
 from base import *
-from os import environ
+
 
 import dj_database_url
 
@@ -7,17 +7,6 @@ DEBUG = False
 TEMPLATE_DEBUG = DEBUG
 
 
-# DATABASES = {
-#     'default': dj_database_url.config(default='postgres://localhost')
-# }
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pcpg',
-        'USER': environ.get('PC_DB_USER') or '',
-        'PASSWORD': environ.get('PC_DB_PASSWORD') or '',
-        'HOST': 'localhost',
-        'PORT': '5432',
-    }
+    'default': dj_database_url.config(default='postgres://localhost')
 }
